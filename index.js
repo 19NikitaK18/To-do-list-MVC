4f9730f82a77d056536e7c0e5f901d8cf8bbf7a0
19NikitaK18 <85367354+19NikitaK18@users.noreply.github.com>
2021-06-04 23:10:10 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+class View {
+    constructor() {
+        this.mainBlock = document.querySelector('#app');
+        this.input = document.createElement('input');
+        this.button = document.createElement('button');
+        this.taskList = document.createElement('ul');
+    }
+
+    initRender() {
+        this.mainBlock.append(this.input, this.button, this.taskList);
+        this.button.innerHTML = 'Add';
+    }
+
+    renderTask(smthCameInputValue) {
+        let task = document.createElement('li');
+        this.taskList.appendChild(task);
+        task.innerHTML = smthCameInputValue;
+        //button
+        let delInput = document.createElement('input');
+        delInput.setAttribute('type', 'button');
+        delInput.setAttribute('value', 'X');
+
+        task.appendChild(delInput);
+
+        this.input.value = '';
+    }
+}
+
+class Controller {
+    constructor(model, view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    addData() {
+        let value = this.view.input.value;
+        if(value !== '') {
+            this.model.addTask(value);
+            this.view.renderTask(value);
+        }else {
+            console.log('nothing');
+        }
+    }
+
+    addHandle() {
+        this.view.button.addEventListener('click', () => this.addData());
+    }
+}
+
+class Model {
+    constructor() {
+        this.tasks = [];
+    }
+
+    addTask(value) {
+        this.tasks.push(value);
+        console.log(this.tasks);
+    }
+}
+
+const view = new View();
+const model = new Model();
+const controller = new Controller(model, view);
+view.initRender();
+controller.addHandle();
